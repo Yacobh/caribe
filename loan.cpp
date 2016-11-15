673f1e43aeea0fd32756ebb2dbda4236b093de42
Yacobh <jacobocordova@gmail.com>
2016-11-15 01:44:47 -0300

    Create loan.cpp

---
+++ loan.cpp
@@ -0,0 +1,38 @@
+/*
+	Name: 
+	Copyright: 
+	Author: 
+	Date: 15-11-16 00:36
+	Description: 
+*/
+
+#include <iostream>
+using namespace std;
+
+
+/*A finance company provides loans for motorcycles at different 
+rates depending on how much the total loan amount is and how 
+many payments will be made on the loan. Using the information 
+in the table below, write a program that will calculate 
+the monthly payment based on user inputs of loan amount and 
+number of monthly payments. The user will NOT input the 
+percentage rate, as this will be determined by the program code 
+based on user input of loan amount and number of payments. 
+
+The output will display the loan amount, number of payments, 
+monthly payments and the interest rate of the loan. Use a loop 
+to allow users to enter as many sets of data as desired. 
+At the end of each loop, ask the user if he or she would like 
+to Exit the program (Y for Yes or N for No). 
+If yes, clear the variables and repeat the input, processing 
+and output loop. If no, exit the program.
+*/
+
+int main()
+{
+		char exit='y'
+		while(exit!='N' || exit!='n') {
+			inputdata();
+			
+}
+}
