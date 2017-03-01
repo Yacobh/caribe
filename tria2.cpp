c05a3bd7faf8258e43419b2a30f1f9e92d57d40e
Yacobh <jacobocordova@gmail.com>
2017-03-01 10:45:22 -0300

    Create tria2.cpp

---
+++ tria2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+	
+  // Declaration of variables
+  char identifier[4];
+  int  running[4], swimming[4], biking[4];
+  int total[4];
+  	for (int i=0; i<4; i++)
+    {
+      identifier[i]=i+'A';
+      running[i]=i*11;
+      swimming[i]=i*10;
+      biking[i]=i*5;
+    }
+  
+  // Name of the file
+  string fileName="racedat.txt";
+  // stream to open the file
+	ifstream fin; 
+  //open the file
+	fin.open (fileName);
+	if(fin.is_open())
+	{
+   // load the data
+		for (int i=0; i<4; i++)
+    {
+      fin >> identifier[i];
+      fin >> running[i];
+      fin >> swimming[i];
+      fin >> biking[i];
+    }
+    
+    //calculate the total time for each competitor
+    for (int i=0; i<4; i++)
+    {
+      total[i]=running[i]+swimming[i]+biking[i];
+    }
+    
+    //print the results
+    //cout << "Competitor\t\tRunning time\t\tSwimming time \t\t Biking Time\t\t Total time " <<endl; 
+	} else cout << "unable to open the file"<<endl;
+	fin.close();
+	cout << "Competitor\t\tRunning time\t\tSwimming time \t\tBiking Time\t\tTotal time " <<endl; 
+	for (int i=0; i< 4; i++)
+	{
+	    cout <<  identifier[i]<<  "\t\t\t"<<  running[i]<<  "\t\t\t"<< swimming[i]<<  "\t\t\t"<< biking[i] <<  "\t\t\t"<<total[i]<< endl;
+	}
+	return 0;
+}
